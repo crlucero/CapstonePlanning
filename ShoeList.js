3758c498f7ddcf382a482d9e1012e67926540dda
Cristian Lucero <cristianjlucero32@gmail.com>
2019-03-01 11:00:50 -0800

    Began adding shoes to list that will be displayed

---
+++ ShoeList.js
@@ -0,0 +1,54 @@
+const shoeList = [
+  {
+    name: " Air Jordan XI 'Concord' ",
+    price: "$220",
+    description:
+      "Michael Jordan is fondly remembered for wearing the Air Jordan XI en route to the NBA Championship in 1996, but he actually debuted the “Concord” colorway during the Bulls’ second-round loss to the Orlando Magic in the 1995 NBA Playoffs. And in that series, Jordan’s PE pair displayed the number 45 on their heels—the number Jordan initially wore upon his return to the league just two months prior. That edition never released to the public until 2018, when it was brought back with further attention to detail like a more accurate reproduction of the cut on the model’s iconic patent leather. The launch wasn’t as chaotic as in past years, but it was a hit on a level that transcends sneakers alone."
+  },
+  {
+    name: "Adidas Yeezy 500",
+    price: "$200",
+    description:
+      "2018 was the year of the Dad Shoe, and there’s no better indication of that fact than Kanye West’s Yeezy line’s departure from thin, packable sock sneakers to chunky, suede models with bulbous soles. The sneaker that everyone was wearing this year, because everyone could finally buy it, was the Yeezy 500. West claims that he sold 250,000 pairs of them in one hour during Coachella. It sure felt that way, as everyone had these on their feet this summer. The resale value reflected that, too, and it wasn’t hard to get a pair on the secondary market for a little over the original retail price. Blush, Salt, Black—you could wear them every day with anything. Regardless of how you feel about someone as a person—if who someone did or didn’t vote for for president drives those reasons, you have bigger issues—a good shoe is a good shoe, and the Yeezy 500 falls under that category."
+  },
+  {
+    name: " Diamond Supply Co. x Nike SB Dunk Low 'Canary'",
+    price: "$100",
+    description: "It’s only fitting that in a year when the Nike SB Dunk began its return to prominence, a collaboration with Diamond Supply Co. would help lead the hype with three new takes on its “Tiffany” Dunk—an instant classic from the heyday of Nike Skateboarding. All three versions features nods to the original, with croc skin debossing on the overlays, silver Swooshes, and hidden “Tiffany” blue detailing. It was the extra-limited “Canary” yellow version, however, that really brought back the feeling of Nike SB in the mid-2000s by starting a mini-riot at ComplexCon that subsequently got the release canceled. The sneakers eventually dropped at a jewelry store pop-up, and have since exploded on the resale market, where they currently average well over $2,000. It remains to be seen if the Dunk SB can make a full-fledged comeback, but there’s no denying Diamond Supply Co. is doing its best to make that happen."
+  },
+  {
+    name: "",
+    price: "",
+    description: ""
+  },
+  {
+    name: "",
+    price: "",
+    description: ""
+  },
+  {
+    name: "",
+    price: "",
+    description: ""
+  },
+  {
+    name: "",
+    price: "",
+    description: ""
+  },
+  {
+    name: "",
+    price: "",
+    description: ""
+  },
+  {
+    name: "",
+    price: "",
+    description: ""
+  },
+  {
+    name: "",
+    price: "",
+    description: ""
+  }
+];
